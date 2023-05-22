226b163fa615e5572d3329192a4b5ba3c3e84ce1
Salini Vijayan <96425954+iamsalinivijayan@users.noreply.github.com>
2023-05-23 01:27:37 +0530

    Second commit

--- Product.py
+++ Product.py
@@ -1,7 +1,11 @@
-from Product import *
-# Class Item
-class Item:
-    def __init__(self, product, quantity,gift_wrap):
-        self.product = product
-        self.quantity = quantity
-        self.gift_wrap= gift_wrap
+# class Product
+class Product:
+    def __init__(self, name, price):
+        self._name = name
+        self._price = price
+
+    def get_name(self):
+        return self._name
+
+    def get_price(self):
+        return self._price
